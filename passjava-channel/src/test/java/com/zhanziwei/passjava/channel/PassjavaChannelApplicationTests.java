b6d263af24c6c55b87859b6908b418918feeb6a4
yufeng <1715024080@qq.com>
2021-01-16 21:44:47 +0800

    项目骨架建立

---
+++ passjava-channel/src/test/java/com/zhanziwei/passjava/channel/PassjavaChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhanziwei.passjava.channel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
